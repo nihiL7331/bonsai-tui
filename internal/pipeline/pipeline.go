8a62d3500c176df7e39c3b487d1bb3a81596d9c9
nihiL7331 <pujanekp6@gmail.com>
2026-04-05 17:08:42 +0200

    feat: atlas packing logic & checkDependencies accepts logFunc

--- internal/pipeline/pipeline.go
+++ internal/pipeline/pipeline.go
@@ -5,7 +5,7 @@ import (
 	"os/exec"
 )
 
-func CheckDependencies() (string, error) {
+func CheckDependencies(logFn func(string, string)) (string, string, error) {
 	requiredTools := map[string]string{
 		"odin": "Odin compiler not found in PATH. Please install it from https://odin-lang.org/docs/install",
 		"emcc": "Could not find Emscripten SDK.\n Please install it (https://emscripten.org/docs/getting_started/downloads.html)\n and set the 'EMSDK' environment variable to its installation folder",
@@ -13,14 +13,19 @@ func CheckDependencies() (string, error) {
 
 	for bin, errStr := range requiredTools {
 		if _, err := exec.LookPath(bin); err != nil {
-			return "", fmt.Errorf(errStr)
+			return "", "", fmt.Errorf(errStr)
 		}
 	}
 
-	shdcPath, err := EnsureShdc()
+	shdcPath, err := EnsureShdc(logFn)
 	if err != nil {
-		return "", fmt.Errorf("Failed to set up sokol-shdc: %w", err)
+		return "", "", fmt.Errorf("Failed to set up sokol-shdc: %w", err)
 	}
 
-	return shdcPath, nil
+	msdfPath, err := EnsureMsdfGen(logFn)
+	if err != nil {
+		return "", "", fmt.Errorf("Failed to set up msdf-atlas-gen: %w", err)
+	}
+
+	return shdcPath, msdfPath, nil
 }
